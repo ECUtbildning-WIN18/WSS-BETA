dd99a64ba7ee360587ed26c07dd4a7d7c20102fd
Stefan Pettersson <trancedude79@hotmail.com>
2018-10-01 10:35:39 +0200

    Adding props and constructor

--- WSS/Domain/PrisonCells.cs
+++ WSS/Domain/PrisonCells.cs
@@ -6,5 +6,36 @@ namespace WSS.Domain
 {
     class PrisonCells
     {
+        public string CellId { get; }
+        public string Block { get; set; }
+        public string CellNumber { get; set; }
+        public int PrisonerBlock { get; set; }
+        public bool Toilet { get; private set; }
+        public bool Shower { get; private set; }
+
+        public PrisonCells(string block, string cellNumber, int prisonerBlock, bool toilet, bool shower)
+        {
+            Block = block;
+            CellNumber = cellNumber;
+            PrisonerBlock = prisonerBlock;
+            CellId = $"{block}-{cellNumber}";
+            Toilet = toilet;
+            Shower = shower;
+        }
+
+        public void InstallToilet()
+        {
+            Toilet = true;
+            Console.Clear();
+            Console.WriteLine("A brand new Toilet is installed");
+        }
+
+        public void InstallShower()
+        {
+            Shower = true;
+            Console.Clear();
+            Console.WriteLine("A brand new Shower is installed");
+        }
+
     }
 }
