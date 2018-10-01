402b9229c3268cf89814ced4073919423d33aee6
Dawid <dawid_owizyc@hotmail.com>
2018-10-01 10:28:26 +0200

    Menu Fixing

--- WSS/Domain/MenuMessages.cs
+++ WSS/Domain/MenuMessages.cs
@@ -10,137 +10,115 @@ namespace WSS.Domain
     {
         public static void MenuMessagesStart()
         {
-            bool menuRun = true;
 
-            do                                                       
-            {
-                Console.Clear();
-                Console.WriteLine("# Wedlock Security Systems #\n");
-                Console.WriteLine("1 Infrastructure");
-                Console.WriteLine("2 Prisoners");
-                Console.WriteLine("3 Wedlock Devices");
-                Console.WriteLine("4 Exit\n\n");
-                
+            Console.Clear();
+            Console.WriteLine("# Wedlock Security Systems #\n");
+            Console.WriteLine("1 Infrastructure");
+            Console.WriteLine("2 Prisoners");
+            Console.WriteLine("3 Wedlock Devices");
+            Console.WriteLine("4 Exit\n\n");
+        }
 
-
-            Int32.TryParse(Console.ReadLine(), out int tal);
-                
-                switch (tal)
-                {
-
-                    case 1:
-                        {
-                            
-                                bool menu2Run = true;
-                                while (menu2Run)
-                                {
-
-                                    Console.Clear();
-                                    Console.WriteLine("# Infrastructure\n");
-                                    Console.WriteLine("1 List Blocks");
-                                    Console.WriteLine("2 Add Block");
-                                    Console.WriteLine("3 Back to main menu\n");
-                                    Int32.TryParse(Console.ReadLine(), out int tal2);
-                                    switch (tal2)
-                                    {
-                                        case 1:
-                                            {
-                                            Console.Clear();
-                                            Console.WriteLine("# List Blocks");
-                                            //code
-
-                                            //code
-                                            Console.ReadLine();
-                                                break;
-                                            }
-
-                                        case 2:
-                                            {
-                                            Console.Clear();
-                                            Console.WriteLine("# Add Block");
-                                            //code
-
-                                            //code
-                                                Console.ReadLine();
-                                                break;
-                                            }
-
-                                        case 3:
-                                            {
-                                            Console.Clear();
-                                            Console.WriteLine("# Back to main menu");
-                                                //Back to main menu
-
-                                                menu2Run = false;
-                                                break;
-                                            }
-
-                                        default:
-                                            Console.WriteLine(" !!! ");
-                                            break;
-                                    }
-
-                                }
-
-                            
-                        
-                        }
-
-                            break;
-
-                    case 2:
-                        {
-                            Console.Clear();
-                            Console.WriteLine("# Prisoners\n");
-                            //code
-
-                            //code
-                            Console.ReadLine();
-                           
-                        }
-
-                        break;
-                    case 3:
-                        {
-                            Console.Clear();
-                            Console.WriteLine("# Wedlock Devices");
-                            //code
-
-                            //code
-                            Console.ReadLine();
-                            
-                        }
-
-                        break;
-
-                    case 4:
-                        {
-                            Console.Clear();
-                            Console.WriteLine("# Wedlock Security Systems Exit");
-                            //code
-                            Thread.Sleep(2000);
-                            Environment.Exit(0);
-                            //code
-                            Console.ReadLine();
-                            
-                        }
-
-                        break;
+        public static void InfrastructureMenu()
+        {
 
 
-                    default:
-                        {
-                            Console.Clear();
-                            Console.WriteLine(" ");
-                            Console.ReadLine();
-                        }
-                        break;
+            Console.Clear();
+            Console.WriteLine("# Infrastructure\n");
+            Console.WriteLine("1 List Blocks");
+            Console.WriteLine("2 Add Block");
+            Console.WriteLine("3 Back to main menu\n");
 
-
-                }
-
-            }while (menuRun) ;
 
         }
+
+        public static void BlockList()
+            {
+                Console.Clear();
+                Console.WriteLine("# List Blocks\n");
+                //code
+
+                Console.Write("Block >");
+                string SelectBlock = Console.ReadLine();
+                Console.WriteLine(SelectBlock);
+
+                // (V)iew block, (D)elete block
+                //
+                //          
+                //              # Block ID_
+                //     
+                //              // 1 List Cell
+                // 2 Add Cell
+                //Delete block [block] (Y)es, (N)o 
+
+                //
+
+                //code
+        }  
+                 
+                 public static void BlocksAdd()
+                 {
+                    //##//
+                    Console.Clear();
+                    Console.WriteLine("# Add Block\n");
+                    //code
+
+
+
+                    //code
+                    
+                    Console.Clear();
+                    Console.WriteLine("# Back to main menu");
+                    MenuMessages.MenuMessagesStart(); //Back to main menu
+
+                    //##//
+                 }
+
+
+
+
+        public static void Prisoners()
+        {
+
+            //##//
+            Console.Clear();
+            Console.WriteLine("# Prisoners\n");
+            //code
+            
+            //code
+            
+
+            //##//
+
+        }
+
+
+
+        public static void WedlockDevices()
+        {
+            //##//
+            Console.Clear();
+            Console.WriteLine("# Wedlock Devices\n");
+            //code
+
+            //code
+            
+            //##//
+        }
+
+        public static void WssExit()
+        {
+            //##//
+            Console.Clear();
+            Console.WriteLine("# Wedlock Security Systems Exit");
+            //code
+            Thread.Sleep(2000);
+            Environment.Exit(0);
+            //##//
+
+
+        }
+
+
     }
 }
