3f63b4f17d4a7a1dc423f9dabc5404cba99c6f82
Philip Englund <phille96@gmail.com>
2018-10-01 10:17:28 +0200

    Added class Device

---
+++ WSS/Device.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WSS
+{
+    class Device
+    {
+        public int BatteryPower { get; set; } = 100;
+
+        public bool MateInRange { get; set; } = false;
+
+        public Prisoner Owner { get; set; }
+
+        public int DeviceMateID { get; }
+
+        public static void BlowUp(Prisoner prisoner)
+        {
+            prisoner.IsAlive = false;
+        }
+
+    }
+}
