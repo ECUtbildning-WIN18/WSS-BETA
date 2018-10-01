2a112bd42a8c76a3a26cf9603c0a217a89326af2
Philip Englund <phille96@gmail.com>
2018-10-01 13:06:53 +0200

    added list

--- WSS/Program.cs
+++ WSS/Program.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using WSS.Domain;
 
 namespace WSS
 {
     class Program
     {
+        public static Dictionary<string, Prisoner> PrisonerList = new Dictionary<string, Prisoner>();
+
+        public static List<PrisonCells> CellsList = new List<PrisonCells>();
+
         static void Main(string[] args)
         {
 
