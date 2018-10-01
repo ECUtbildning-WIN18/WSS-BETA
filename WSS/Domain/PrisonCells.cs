94f02efca0be55f36c45bc435351e5325ce826ca
Stefan Pettersson <trancedude79@hotmail.com>
2018-10-01 10:41:32 +0200

    Adding  PrisonerSlot

--- WSS/Domain/PrisonCells.cs
+++ WSS/Domain/PrisonCells.cs
@@ -7,18 +7,20 @@ namespace WSS.Domain
     class PrisonCells
     {
         public string CellId { get; }
+        public string PrisonerSlot { get; set; }
         public string Block { get; set; }
         public string CellNumber { get; set; }
-        public int PrisonerBlock { get; set; }
+        public int BedNumber { get; set; }
         public bool Toilet { get; private set; }
         public bool Shower { get; private set; }
 
-        public PrisonCells(string block, string cellNumber, int prisonerBlock, bool toilet, bool shower)
+        public PrisonCells(string block, string cellNumber, int bedNumber, bool toilet, bool shower)
         {
+            CellId = $"{block}-{cellNumber}";
+            PrisonerSlot = $"{block}-{cellNumber}:{bedNumber}";
             Block = block;
             CellNumber = cellNumber;
-            PrisonerBlock = prisonerBlock;
-            CellId = $"{block}-{cellNumber}";
+            BedNumber = bedNumber;
             Toilet = toilet;
             Shower = shower;
         }
